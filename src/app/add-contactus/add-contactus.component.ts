1987a448f1fbe0feb924f2e42e105db8dcbe41a1
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-30 22:12:51 +0530

    update

--- src/app/add-contactus/add-contactus.component.ts
+++ src/app/add-contactus/add-contactus.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AddContactusComponent {
 
+  name=""
+  designation=""
+  email=""
+  phone=""
+  subject=""
+
+  readValue=()=>
+  {
+    let data:any={"name":this.name,"designation":this.designation,"email":this.email,"phone":this.phone,"subject":this.subject}
+    console.log(data)
+    this.name=""
+    this.designation=""
+    this.email=""
+    this.phone=""
+    this.subject=""
+  }
 }
